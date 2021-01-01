bbb222ef5de2c7df50494ce524bdc13adced2bf9
Patrick Uhlmann <git@uhlme.ch>
2021-01-01 23:38:01 +0100

    Add Alias for Localhost, fix exposure SECRET_KEY
    - 127.0.0.1 is an alias for localhost. As it is used in the link that is displayed by the manage.py runserver command, it should also be allowed
    - The SECRET_KEY should not be exposed, better to read it from an environment variable.

--- gartenberg/settings.py
+++ gartenberg/settings.py
@@ -10,11 +10,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8cd-j&jo=-#ecd1jjulp_s*7y$n4tad(0d_g)l=6@n^r8fg3rn'
+SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 
 DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'True')=='True'
 
-ALLOWED_HOSTS = ['my.gartenberg.ch','gartenberg.juntagrico.science', 'localhost',]
+ALLOWED_HOSTS = ['my.gartenberg.ch','gartenberg.juntagrico.science', 'localhost', '127.0.0.1']
 
 
 # Application definition
